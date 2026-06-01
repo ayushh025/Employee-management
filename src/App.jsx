25732b08bd284433d7b905bf14e21144f4953d29
ayushh025 <ayushprj12@gmail.com>
2026-06-01 21:58:44 +0530

    Login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
+import Login from "./components/Auth/Login";
+import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard";
 
 const App = () => {
   return (
     <>
-      <h1>App</h1>
+      {/* <Login /> */}
+      <EmployeeDashboard />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Auth/Login.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState({});
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newErr = {};
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!email.trim()) {
+      newErr.email = "Email is required";
+    } else if (!emailRegex.test(email)) {
+      newErr.email = "Please enter a valid email address";
+    }
+    if (!password) {
+      newErr.password = "Password is required";
+    } else if (password.length < 6) {
+      newErr.password = "Password must be at least 6 characters";
+    }
+
+    setErrors(newErr);
+
+    if (Object.keys(newErr).length === 0) {
+      //LOGIC
+      console.log("Email is", email);
+      console.log("Password is", password);
+
+      setEmail("");
+      setPassword("");
+    }
+  };
+
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <div className="border-2 rounded-xl border-emerald-600 w-[95vw] sm:w-auto sm:p-20 p-6">
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col justify-center items-center"
+        >
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            type="text"
+            className="border-2 border-emerald-600 rounded-full py-3 px-5 text-xl text-black outline-none bg-transparent w-full"
+            autoComplete="email"
+            placeholder="Enter your email"
+          />
+          {errors.email && (
+            <span className="text-red-500 text-md mt-1">{errors.email}</span>
+          )}
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            type="password"
+            className="border-2 border-emerald-600 rounded-full py-3 px-5 text-xl text-black outline-none bg-transparent mt-4 w-full"
+            autoComplete="current-password"
+            placeholder="Enter password"
+          />
+          {errors.password && (
+            <span className="text-red-500 text-md mt-1">{errors.password}</span>
+          )}
+          <button
+            className="bg-emerald-600 rounded-full py-3 px-5 text-xl text-white outline-none mt-6 border-none w-1/2"
+            placeholder="Enter password"
+          >
+            Log in
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
